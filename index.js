19229096cecd80192b3bdf42657831e7db1ac9a9
alifbinhossain <you@example.com>
2021-10-27 02:02:06 +0600

    first commit with volunteer server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwgkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function server() {
+  try {
+    await client.connect();
+
+    const database = client.db("volunteer_db");
+    const volunteerCollection = database.collection("volunteers");
+  } finally {
+    // await client.close();
+  }
+}
+
+server().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from volunteer server");
+});
+
+app.listen(port, () => {
+  console.log("Server running on", port);
+});
